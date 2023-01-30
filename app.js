bc176ac7766519dea984892a8bd61899efa94294
BigGrayCat22 <dpavlovjwork@gmail.com>
2023-01-30 16:04:33 +0200

    Закрепляем работу с Git по ходу пьессы )

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-console.log('hello world');
+/*let a = 3222;
+console.log(a);
+a = 'test';
+console.log(a); Lesson11 
+
+
+const width = 10;
+const height = 5;
+const space = width * height;
+console.log(space);
+const newWidth = width - 4;
+const newWidth2 = width + 3;
+console.log(newWidth2);
+const division = newWidth / newWidth2;
+console.log(division);
+const volume = 2 ** 3; /// Возведение в степень.
+console.log(volume);
+
+// Строки
+const city = 'Cherkassy';
+const street = 'Vetnihora';
+console.log(city + ', ' + street + ' ' + 5); Lesson12 
+
+//операторы присваивания
+let age = 38;
+age += 2;
+age++;
+age *= 2;
+age /= 3;
+console.log(age);
+
+//операторы сравнения
+const vasia = 20;
+console.log(age > vasia);
+console.log(age >= vasia);
+console.log(age < vasia);
+console.log(age <= vasia);
+console.log(age ==   vasia); Lesson 13 */
+
+// Порядок вычислений
+const isSuited = 100 - 10 > 90 - 5;
+console.log(isSuited);
+const a = (5 + 10) / 2; //группировка; (Скобки)
+console.log(a);
+let b;
+let c;
+c = b = 100 + 50;
+console.log(c);
+console.log(b);
